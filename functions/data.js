e124feacd9b06f71ade710907864b06a3cb15b57
zilkupcin <zil.kupcin@gmail.com>
2022-01-16 12:38:19 +0000

    Move data to a new file

---
+++ functions/data.js
@@ -0,0 +1,22 @@
+const iconSet = [
+  "football-ball",
+  "mountain",
+  "tree",
+  "wind",
+  "tractor",
+  "space-shuttle",
+  "meteor",
+  "rocket",
+  "bomb",
+  "cloud",
+  "feather",
+  "bone",
+  "fish",
+  "ice-cream",
+  "pizza-slice",
+  "stroopwafel",
+  "plane",
+  "wine-glass-alt",
+];
+
+exports.iconSet = iconSet;
